72e3c35feb648ce6924b8f10af10f165c1ee01b2
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-09 15:19:36 -0500

    created get_users_move method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,19 @@
 require 'pry'
 class Turn
+  attr_accessor :users_move
+  def initialize
+    @users_move = ''
+  end
   def prompt(player)
     puts "It's your turn #{player.name}. Where would you like to move?"
   end
 
   def show_available_moves(array)
     array.each{|move| puts move}
+  end
+
+  def get_users_move
+    move = gets.chomp
+    @users_move = move
   end
 end
