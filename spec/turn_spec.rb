cca7577ee4f6478dee90b83cc5038a84b9df29aa
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-09 13:42:04 -0500

    created prompt test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,10 +1,16 @@
 require_relative 'spec_helper'
 
 describe Turn do
+  let(:turn) {Turn.new}
+  let(:player) {Player.new}
 
   it "should created a new turn object with Turn.new" do
-    turn = Turn.new
     expect(turn.class).to eq(Turn)
+  end
+
+  it "should prompt a player to make a move" do
+    expect(STDOUT).to receive(:puts).with("It's your turn Player. Where would you like to move?")
+    turn.prompt(player)
   end
 
 end
