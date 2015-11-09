c6b17734afa5ee21a5c829c1425a1a3d9b8302d0
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-09 14:11:41 -0500

    created a test to show available moves

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -13,4 +13,9 @@ describe Turn do
     turn.prompt(player)
   end
 
+  it "should take an array of available moves and display them to the user" do
+    expect(STDOUT).to receive(:puts).with("1,1\n1,2\n2,0\n")
+    turn.show_available_moves(["1,1","1,2","2,0"])
+  end
+
 end
