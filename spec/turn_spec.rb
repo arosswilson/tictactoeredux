eec158d5738482dd31105d4f5f5941ae9c9d2879
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-09 21:15:20 -0500

    create verify move is available test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -15,13 +15,19 @@ describe Turn do
 
   it "should take an array of available moves and display them to the user" do
     expect(STDOUT).to receive(:puts).with("1,1")
-    turn.show_available_moves(["1,1"])
+    expect(STDOUT).to receive(:puts).with("2,2")
+    turn.show_available_moves(["1,1", "2,2"])
   end
 
-  it "should ensure that the user's move is valid" do
+  it "should get input from a user and set users_move" do
     allow(turn).to receive(:gets).and_return("1,1")
     turn.get_users_move
     expect(turn.users_move).to eq("1,1")
+  end
+
+  it "should validate that the user's selection is in the array of available moves" do
+    allow(turn).to receive(:gets).and_return("1,1")
+    expect(turn.verify_move_is_available(["1,1", "2,2"])).to be_truthy
   end
 
 end
