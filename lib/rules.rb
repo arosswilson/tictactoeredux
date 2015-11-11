d559c1a82221caedba631ca376eb8e2dda339f74
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-11 12:16:26 -0500

    add initialize and col check

--- lib/rules.rb
+++ lib/rules.rb
@@ -1,9 +1,19 @@
 class Rules
-  def row_check(board = @board)
-    board.each do |row|
+  attr_reader :board, :board_arr
+  def initialize(args={})
+    @board = args[:board]
+    @board_arr = @board.board
+  end
+
+  def row_check(board_arr = @board_arr)
+    board_arr.each do |row|
       return row[0] if row.uniq.length == 1 && !(row.include?('_'))
     end
     nil
+  end
+
+  def col_check
+    row_check(@board_arr.transpose)
   end
 
 end
