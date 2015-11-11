6739a154e6743f350a3898ea21c029a624036ba4
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-11 11:36:19 -0500

    created a new row_check test

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -4,6 +4,7 @@ describe Rules do
   let(:rules) {Rules.new}
   let(:board) {Board.new({board_size: 3, board: [["_","X","O"],["O","X","_"],["_","O","X"]]})}
   let(:winner) {Board.new({board_size: 3, board: [["X","_","O"],["O","X","_"],["_","O","X"]]})}
+  let(:row_winner) {Board.new({board_size: 3, board: [["X","X","X"],["O","O","_"],["_","O","X"]]})}
 
   it "should created a new rules object with Rules.new" do
     expect(rules.class).to eq(Rules)
@@ -12,6 +13,10 @@ describe Rules do
   describe "row checks" do
     it "should return nil on a row check if there isn't a winner" do
       expect(rules.row_check(board.board)).to eq(nil)
+    end
+
+    it "should return the winning piece if the board has a row winner" do
+      expect(rules.row_check(row_winner.board)).to eq("X")
     end
   end
 end
