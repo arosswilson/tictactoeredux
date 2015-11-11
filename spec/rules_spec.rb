16950e6e25e166a2e5f3ab66cf42dfbc4472c984
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-11 11:29:58 -0500

    create a row check test

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -2,8 +2,16 @@ require_relative 'spec_helper'
 
 describe Rules do
   let(:rules) {Rules.new}
+  let(:board) {Board.new({board_size: 3, board: [["_","X","O"],["O","X","_"],["_","O","X"]]})}
+  let(:winner) {Board.new({board_size: 3, board: [["X","_","O"],["O","X","_"],["_","O","X"]]})}
 
   it "should created a new rules object with Rules.new" do
     expect(rules.class).to eq(Rules)
+  end
+
+  describe "row checks" do
+    it "should return nil on a row check if there isn't a winner" do
+      expect(rules.row_check(board)).to eq(nil)
+    end
   end
 end
