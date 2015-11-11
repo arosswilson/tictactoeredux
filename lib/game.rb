b49159ecf7b1d013114cc15e89a4576f7e3036e6
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-11 17:09:04 -0500

    created initialize for Game

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,8 @@
 class Game
+  attr_accessor :board, :rules
+  def initialize(args={})
+    @board = args[:board] || Board.new({board_size: 3})
+    @rules = args[:rules] || Rules.new({board: board})
+  end
 
 end
