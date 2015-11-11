c48a538791193541f09c722b79389e0b7696895e
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-11 17:43:32 -0500

    create clear method

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,17 @@
 class Game
-  attr_accessor :board, :rules
+  attr_accessor :board, :rules, :players
   def initialize(args={})
     @board = args[:board] || Board.new({board_size: 3})
     @rules = args[:rules] || Rules.new({board: board})
+    @players = args[:players] || []
   end
 
   def welcome_message
     puts "Welcome to Tic-Tac-Toe!"
+  end
+
+  def clear
+    puts "\e[H\e[2J"
   end
 
 end
