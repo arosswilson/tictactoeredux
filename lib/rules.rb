864d46cc9a70d76a29dad52e244daa07feb71794
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-11 13:15:49 -0500

    created the diagonal check and updated the isntance variables

--- lib/rules.rb
+++ lib/rules.rb
@@ -1,19 +1,26 @@
 class Rules
-  attr_reader :board, :board_arr
+  attr_reader :board, :board_obj
   def initialize(args={})
-    @board = args[:board]
-    @board_arr = @board.board
+    @board_obj = args[:board]
+    @board = @board_obj.board
   end
 
-  def row_check(board_arr = @board_arr)
-    board_arr.each do |row|
+  def row_check(board = @board)
+    board.each do |row|
       return row[0] if row.uniq.length == 1 && !(row.include?('_'))
     end
     nil
   end
 
   def col_check
-    row_check(@board_arr.transpose)
+    row_check(@board.transpose)
+  end
+
+  def diag_check
+    diag_one = (0...3).collect{|i| board[i][i]}
+    diag_two = (1..3).collect{|i| board[-i][(i-1)]}
+    return diag_one[0] if diag_one.uniq.length == 1 && !(diag_one.include?('_'))
+    return diag_two[0] if diag_two.uniq.length == 1 && !(diag_two.include?('_'))
   end
 
 end
