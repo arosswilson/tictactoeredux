17fd2ff5780b5468b392069e28e186f75d7f1123
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-12 18:44:45 -0500

    moving board updates to turn

--- lib/human.rb
+++ lib/human.rb
@@ -13,13 +13,13 @@ class Human < Player
     "Human"
   end
 
-  def move(board)
-    display("It's your turn, #{name}. Please pick from the possible moves \nThe first choice is the row, the second is the column")
-    @last_move = disp_and_res(board.possible_moves)
-    until board.validate_move(@last_move)
-      display("that choice was invalid, please pick a valid choice from the list of possible moves:")
-      @last_move = disp_and_res(board.possible_moves)
-    end
-    board.move(marker, @last_move)
-  end
+  # def move(board)
+  #   display("It's your turn, #{name}. Please pick from the possible moves \nThe first choice is the row, the second is the column")
+  #   @last_move = disp_and_res(board.possible_moves)
+  #   until board.validate_move(@last_move)
+  #     display("that choice was invalid, please pick a valid choice from the list of possible moves:")
+  #     @last_move = disp_and_res(board.possible_moves)
+  #   end
+  #   board.move(marker, @last_move)
+  # end
 end
