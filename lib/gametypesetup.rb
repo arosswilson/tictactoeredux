4dfb77fa9b4b161697f8b879eb43b14d45384440
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-13 16:03:31 -0500

    remove the functionality that has moved to playersetup

--- lib/gametypesetup.rb
+++ lib/gametypesetup.rb
@@ -6,8 +6,6 @@ class GametypeSetup #consider extracting update player info and choose order to 
     clear
     welcome_message
     get_game_type(game)
-    update_player_info(game)
-    choose_order(game)
   end
 
   def self.welcome_message
@@ -41,21 +39,5 @@ class GametypeSetup #consider extracting update player info and choose order to 
   def self.display_message_and_get_response(message)
     puts message
     return gets.chomp
-  end
-
-  def self.choose_order(game)
-    response =''
-    until response == '1' || response == '2'
-      response = display_message_and_get_response("Please enter the number for the player that you want to go first: \n 1. #{game.players[0].class} #{game.players[0].marker} \n 2. #{game.players[1].class} #{game.players[1].marker}")
-    end
-    game.players.reverse! if response == '2'
-  end
-
-  def self.update_player_info(game)
-    game.players.each_with_index do |player, i|
-      puts "For #{player.class.superclass} #{(i+1)}: #{player.class} \n"
-      player.get_info(game.players[i-1].marker)
-      game.players[i-1].update_op_mrkr(player.marker)
-    end
   end
 end
