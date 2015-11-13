7c1709e880ef83b19cacbec2003cc5a56d44f6a5
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-13 15:59:45 -0500

    create a playersetup spec with the two tests

---
+++ spec/playersetup_spec.rb
@@ -0,0 +1,32 @@
+require_relative 'spec_helper'
+
+describe PlayerSetup do
+  let(:playersetup) {PlayerSetup.new}
+  let(:game) {Game.new}
+  let(:gametypesetup) {Gametypesetup.new}
+
+  it "should have the class 'PlayerSetup'" do
+    expect(playersetup.class).to eq(PlayerSetup)
+  end
+
+  it "should change the order of the players based on user input" do
+    allow(GametypeSetup).to receive(:gets).and_return("3")
+    allow(PlayerSetup).to receive(:gets).and_return("2")
+    GametypeSetup.get_game_type(game)
+    PlayerSetup.choose_order(game)
+    expect(game.players[0]).to be_a(Computer)
+    expect(game.players[1]).to be_a(Human)
+  end
+
+  it "should get player info for each player" do
+    allow(GametypeSetup).to receive(:display_message_and_get_response).and_return("1")
+    GametypeSetup.get_game_type(game)
+    allow(game.players[0]).to receive(:display_message_and_get_response).and_return("Ross","V")
+    allow(game.players[1]).to receive(:display_message_and_get_response).and_return("Miles","O")
+    PlayerSetup.update_player_info(game)
+    expect(game.players[0]).to be_a(Human)
+    expect(game.players[1]).to be_a(Human)
+    expect(game.players[0].name).to eq("Ross")
+    expect(game.players[1].name).to eq("Miles")
+  end
+end
