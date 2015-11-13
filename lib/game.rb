376e9c9c5d9f3c927147becdad5c2d21a9f1cb59
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-13 16:39:07 -0500

    add keep_playing loop

--- lib/game.rb
+++ lib/game.rb
@@ -4,19 +4,23 @@ require_relative 'turn'
 require_relative 'gametypesetup'
 require_relative 'playersetup'
 class Game
-  attr_accessor :board, :rules, :players, :turn
+  attr_accessor :board, :rules, :players, :turn, :keep_playing
   attr_reader :moves
   def initialize(args={})
     @board = args[:board] || Board.new({board_size: 3})
     @rules = args[:rules] || Rules.new({board: board})
     @players = args[:players] || []
     @turn = args[:turn] || Turn.new
+    @keep_playing = true
   end
 
   def run
     get_started
-    game_loop
-    find_winner
+    while keep_playing
+      game_loop
+      find_winner
+      play_again?
+    end
   end
 
   def get_started
@@ -56,5 +60,32 @@ class Game
 
   def clear
     puts "\e[H\e[2J"
+  end
+
+  def play_again?
+    res = display_message_and_get_response("want to play again? Please enter 'Y' or 'N'").downcase
+    until res == "y" || res == "n"
+      res = display_message_and_get_response("invalid response, please enter 'Y' or 'N'.").downcase
+    end
+    end_game?(res)
+  end
+
+  def end_game?(response)
+    if response == 'n'
+      @keep_playing = false
+    else
+      start_new_game_loop
+    end
+  end
+
+  def display_message_and_get_response(message)
+    puts message
+    return gets.chomp
+  end
+
+  def start_new_game_loop
+    @board = Board.new
+    @rules = Rules.new({board: @board})
+    @turn = Turn.new
   end
 end
