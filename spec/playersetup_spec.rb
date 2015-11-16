a307988756582e3a650c9046f5f5706a4a038ef1
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-15 23:07:25 -0500

    add a test for the opponent marker update

--- spec/playersetup_spec.rb
+++ spec/playersetup_spec.rb
@@ -24,6 +24,18 @@ describe PlayerSetup do
     expect(game.players[1].name).to eq("Miles")
   end
 
+  it "should update opponent marker for computers" do
+    allow(GametypeSetup).to receive(:display_message_and_get_response).and_return("3")
+    GametypeSetup.get_game_type(game)
+    allow(PlayerSetup).to receive(:display_message_and_get_response).and_return("Ross","V","Miles","O", "1")
+    PlayerSetup.run(game)
+    expect(game.players[0]).to be_a(Human)
+    expect(game.players[1]).to be_a(Computer)
+    expect(game.players[0].name).to eq("Ross")
+    expect(game.players[1].name).to eq("Miles")
+    expect(game.players[1].opponent_marker).to eq("V")
+  end
+
   it "should get check to make sure a player's marker isn't being used" do
     allow(PlayerSetup).to receive(:display_message_and_get_response).and_return("X","O")
     PlayerSetup.get_marker(human, ["X"])
