ed2f817d65dd2d4705880910675b9d1d07ea8543
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-15 23:00:00 -0500

    move update player methods to player setup

--- spec/playersetup_spec.rb
+++ spec/playersetup_spec.rb
@@ -2,7 +2,8 @@ require_relative 'spec_helper'
 
 describe PlayerSetup do
   let(:game) {Game.new}
-
+  let(:human) {Human.new}
+  let(:playersetup) {PlayerSetup.new}
   it "should change the order of the players based on user input" do
     allow(GametypeSetup).to receive(:gets).and_return("3")
     allow(PlayerSetup).to receive(:gets).and_return("2")
@@ -15,12 +16,17 @@ describe PlayerSetup do
   it "should get player info for each player" do
     allow(GametypeSetup).to receive(:display_message_and_get_response).and_return("1")
     GametypeSetup.get_game_type(game)
-    allow(game.players[0]).to receive(:display_message_and_get_response).and_return("Ross","V")
-    allow(game.players[1]).to receive(:display_message_and_get_response).and_return("Miles","O")
+    allow(PlayerSetup).to receive(:display_message_and_get_response).and_return("Ross","V","Miles","O")
     PlayerSetup.update_player_info(game)
     expect(game.players[0]).to be_a(Human)
     expect(game.players[1]).to be_a(Human)
     expect(game.players[0].name).to eq("Ross")
     expect(game.players[1].name).to eq("Miles")
+  end
+
+  it "should get check to make sure a player's marker isn't being used" do
+    allow(PlayerSetup).to receive(:display_message_and_get_response).and_return("X","O")
+    PlayerSetup.get_marker(human, ["X"])
+    expect(human.marker).to eq("O")
   end
 end
