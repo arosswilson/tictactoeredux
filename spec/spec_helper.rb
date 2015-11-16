c95302fe95a946ad3e596e1f1eeb918f551b8d92
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-15 23:27:58 -0500

    remove the controller require_relative

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require_relative '../lib/board'
 require_relative '../lib/computer'
 require_relative '../lib/human'
-require_relative '../lib/controller'
 require_relative '../lib/turn'
 require_relative '../lib/rules'
 require_relative '../lib/game'
