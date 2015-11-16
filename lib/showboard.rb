15cc0c20df45ad3051944adb63979219fedfd778
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-16 12:18:41 -0500

    created show board methods

--- lib/showboard.rb
+++ lib/showboard.rb
@@ -1,3 +1,29 @@
 class ShowBoard
+  def self.run(board)
+    clear
+    puts stringify(board)
+  end
 
+  private
+
+  def self.stringify(board)
+    top_row = create_the_top_display_row(board)
+    display = add_additional_board_rows(top_row, board)
+    return display
+  end
+
+  def self.create_the_top_display_row(board)
+    " " +('0'...(board.board_size.to_s)).to_a.join(' ') + "\n"
+  end
+
+  def self.add_additional_board_rows(top_row, board)
+    board.board.each.with_index do |row, i|
+      top_row += (i.to_s + row.join("|")+ "\n")
+    end
+    top_row
+  end
+
+  def self.clear
+    puts "\e[H\e[2J"
+  end
 end
