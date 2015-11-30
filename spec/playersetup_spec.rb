ea795ead06cfbef61fd588d138659b8fa7474439
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-11-30 15:17:03 -0500

    update playersetup specs to expect it to take an array of players

--- spec/playersetup_spec.rb
+++ spec/playersetup_spec.rb
@@ -5,19 +5,19 @@ describe PlayerSetup do
   let(:human) {Human.new}
   let(:playersetup) {PlayerSetup.new}
   it "should change the order of the players based on user input" do
-    allow(game).to receive(:gets).and_return("3")
-    allow(PlayerSetup).to receive(:gets).and_return("2")
-    game.gamesetup.get_game_type
+    allow(game.gamesetup).to receive(:gets).and_return("3")
+    allow(playersetup).to receive(:gets).and_return("2")
+    game.players = game.gamesetup.get_game_type
     playersetup.choose_order(game.players)
     expect(game.players[0]).to be_a(Computer)
     expect(game.players[1]).to be_a(Human)
   end
 
   it "should get player info for each player" do
-    allow(GametypeSetup).to receive(:display_message_and_get_response).and_return("1")
-    GametypeSetup.get_game_type(game)
-    allow(PlayerSetup).to receive(:display_message_and_get_response).and_return("Ross","V","Miles","O")
-    PlayerSetup.update_player_info(game)
+    allow(game.gamesetup).to receive(:display_message_and_get_response).and_return("1")
+    game.players = game.gamesetup.get_game_type
+    allow(playersetup).to receive(:display_message_and_get_response).and_return("Ross","V","Miles","O")
+    playersetup.update_player_info(game.players)
     expect(game.players[0]).to be_a(Human)
     expect(game.players[1]).to be_a(Human)
     expect(game.players[0].name).to eq("Ross")
@@ -25,10 +25,10 @@ describe PlayerSetup do
   end
 
   it "should update opponent marker for computers" do
-    allow(GametypeSetup).to receive(:display_message_and_get_response).and_return("3")
-    GametypeSetup.get_game_type(game)
-    allow(PlayerSetup).to receive(:display_message_and_get_response).and_return("Ross","V","Miles","O", "1")
-    PlayerSetup.run(game)
+    allow(game.gamesetup).to receive(:display_message_and_get_response).and_return("3")
+    game.players = game.gamesetup.get_game_type
+    allow(playersetup).to receive(:display_message_and_get_response).and_return("Ross","V","Miles","O", "1")
+    game.playersetup.run(game.players)
     expect(game.players[0]).to be_a(Human)
     expect(game.players[1]).to be_a(Computer)
     expect(game.players[0].name).to eq("Ross")
@@ -37,8 +37,8 @@ describe PlayerSetup do
   end
 
   it "should get check to make sure a player's marker isn't being used" do
-    allow(PlayerSetup).to receive(:display_message_and_get_response).and_return("X","O")
-    PlayerSetup.get_marker(human, ["X"])
+    allow(playersetup).to receive(:display_message_and_get_response).and_return("X","O")
+    playersetup.get_marker(human, ["X"])
     expect(human.marker).to eq("O")
   end
 end
